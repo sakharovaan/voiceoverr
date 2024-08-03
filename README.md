447d4ae98488558fd6511f664ed402bb60fe3c7f
root <root@DESKTOP-ISTCU9R>
2024-08-03 04:31:24 +0300

    silero

--- README.md
+++ README.md
@@ -4,6 +4,13 @@ Simple script for generating tts voices using coqui based on "screenplay"
 
 Easy howto
 
+For English text (vats-vits):
+
 1. Run docker container `docker run -d -v $(pwd):/voices -v $(pwd):/root/.local/share/tts/ --name coqui --entrypoint /bin/sleep ghcr.io/coqui-ai/tts-cpu 100d`
 2. `docker exec -it coqui /bin/bash`
 3. `python3 /voices/run.py`
+
+
+For Russian text (silero):
+1. `pip install torch`
+2. `python3 run-ru.py`
---
+++ run-ru.py
@@ -0,0 +1,41 @@
+import torch
+import os
+import yaml
+
+
+CONFIG_PATH = 'screenplay-ru.yaml'
+OUTPUT_PATH = 'voices'
+
+
+config = yaml.safe_load(open(CONFIG_PATH))
+lines = config['text'].splitlines()
+
+
+device = torch.device('cuda') # change to cpu
+local_file = 'v4_ru.pt'
+
+if not os.path.isfile(local_file):
+    torch.hub.download_url_to_file('https://models.silero.ai/models/tts/ru/v4_ru.pt',
+                                   local_file)  
+
+model = torch.package.PackageImporter(local_file).load_pickle("tts_models", "model")
+model.to(device)
+
+
+for i, line in enumerate(lines):
+    speaker = line.split(' ')[0]
+    text = ' '.join(line.split(' ')[1:])
+    voice = config['actor_mappings'][speaker]
+    
+    filename = f'{i}_{speaker}_'
+    for i in text:
+        if i.isalpha():
+            filename = "".join([filename,i])
+    filename += '.wav'
+
+    print(f"{speaker} ({voice}): {text} -> {filename}")
+
+    ssml_text = f"<speak><p>{text}</p></speak>"
+    print(ssml_text)
+
+    audio_paths = model.save_wav(ssml_text=ssml_text, speaker=voice, sample_rate=48000, audio_path=f"{OUTPUT_PATH}/{filename}")
